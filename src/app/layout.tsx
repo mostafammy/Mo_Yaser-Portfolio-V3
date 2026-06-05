c736573f9c336f04e49d58a25ac497520001ee69
Mostafa Yaser <mostafa.mmy.10@gmail.com>
2026-06-05 12:30:29 +0300

    fix: prevent horizontal scroll on body with overflow-x-hidden

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col overflow-x-hidden">
         <ThemeProvider>
           <LenisProvider>{children}</LenisProvider>
         </ThemeProvider>
